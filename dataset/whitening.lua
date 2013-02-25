eb8b284cf42a53c1c621853cb553089af2c77abe
Andreas Fidjeland <andreas.fidjeland@gmail.com>
2013-02-25 11:43:34 +0000

    zca_whiten accepts existing whitening matrix

--- dataset/whitening.lua
+++ dataset/whitening.lua
@@ -4,7 +4,9 @@ require 'dataset'
 
 
 -- ZCA-Whitening
-function dataset.zca_whiten(data)
+function dataset.zca_whiten(data, _P)
+
+	local P = _P
 
 	local dims = data:size()
 	local nsamples = dims[1]
@@ -12,11 +14,13 @@ function dataset.zca_whiten(data)
 
 	local mdata = data:clone():reshape(nsamples, n_dimensions)
 	mdata:add(torch.ger(torch.ones(nsamples), torch.mean(mdata, 1):squeeze()):mul(-1))
-	local ce, cv = unsup.pcacov(data:reshape(nsamples, n_dimensions))
-	local val = ce:clone():add(1e-1):sqrt():pow(-1)
-	local diag = torch.diag(val)
-	local P = torch.mm(cv, diag)
-	P = torch.mm(P, cv:t())
+	if not P then
+		local ce, cv = unsup.pcacov(data:reshape(nsamples, n_dimensions))
+		local val = ce:clone():add(1e-1):sqrt():pow(-1)
+		local diag = torch.diag(val)
+		P = torch.mm(cv, diag)
+		P = torch.mm(P, cv:t())
+	end
 	local wdata = torch.mm(mdata, P)
 	return wdata:reshape(dims[1], dims[2], dims[3], dims[4]):typeAs(data), P
 end
